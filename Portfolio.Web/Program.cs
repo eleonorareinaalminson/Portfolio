6084e3bf7b4a641305bc999d2406504fb396955d
Eleonora Alminson <ellialminson@me.com>
2025-05-29 16:18:57 +0200

    Revert "Refactor IndexModel and enhance caching mechanisms"

--- Portfolio.Web/Program.cs
+++ Portfolio.Web/Program.cs
@@ -1,4 +1,3 @@
-// Portfolio.Web/Program.cs
 using Microsoft.EntityFrameworkCore;
 using Portfolio.DataAccessLayer.Data;
 using Portfolio.DataAccessLayer.Repositories;
@@ -6,30 +5,34 @@ using Portfolio.Web.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 
-// Database configuration
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+// Database konfiguration
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Repository pattern
-builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
-
-// HTTP Services
+// Konfigurera HttpClient för tjänsterna (detta registrerar automatiskt tjänsterna också)
 builder.Services.AddHttpClient<ProjectsService>();
 builder.Services.AddHttpClient<WeatherService>();
 
-// Email service
+// Registrera övriga tjänster
 builder.Services.AddScoped<IEmailService, EmailService>();
-
-// Add Response Caching
-builder.Services.AddResponseCaching();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Kör migrations på Azure
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider
+        .GetRequiredService<ApplicationDbContext>();
+    if (dbContext.Database.IsRelational())
+    {
+        dbContext.Database.Migrate();
+    }
+}
+
+// Konfigurera HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
@@ -38,15 +41,9 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
-// Use Response Caching
-app.UseResponseCaching();
-
 app.UseAuthorization();
-
-app.MapRazorPages();
 app.MapControllers();
+app.MapRazorPages();
 
 app.Run();
